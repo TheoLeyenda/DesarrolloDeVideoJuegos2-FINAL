f6756b84e2ae83d1b73d56e1bb5fe3497c6fd1e1
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-05 04:11:24 -0300

    Movimiento de la ranita

---
+++ DesarrolloDeVideoJuegos2-FINAL/Assets/Scripts/Player/MovimientoJugador.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovimientoJugador : MonoBehaviour {
+
+    // Use this for initialization
+    public Camera refCamara;
+    public GameObject refJugador;
+    private Vector3 direcion;
+    public float velocidadJugador;
+    private float x;
+    private float y;
+	void Start () {
+        x = refJugador.transform.position.x;
+        y = refJugador.transform.position.y;
+        //transform.Rotate(new Vector3(90, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow) && refCamara.WorldToScreenPoint(transform.position).y<refCamara.pixelHeight-20)
+        {
+            y = y + velocidadJugador;
+            direcion = new Vector3(x, y, 0);
+            refJugador.transform.position = direcion;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && refCamara.WorldToScreenPoint(transform.position).x>30)
+        {
+            x = x - velocidadJugador;
+            direcion = new Vector3(x, y, 0);
+            refJugador.transform.position = direcion;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && refCamara.WorldToScreenPoint(transform.position).x < refCamara.pixelWidth-30)
+        {
+            x = x + velocidadJugador;
+            direcion = new Vector3(x, y, 0);
+            refJugador.transform.position = direcion;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && refCamara.WorldToScreenPoint(transform.position).y > 20)
+        {
+            y = y - velocidadJugador;
+            direcion = new Vector3(x, y, 0);
+            refJugador.transform.position = direcion;
+        }
+       // direcion = new Vector3(x, 0, z);
+        //refJugador.transform.position = direcion;
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "FinNivel")
+        {
+            SceneManager.LoadScene("Texto_Nivel2");
+        }
+    }
+}
+
---
+++ DesarrolloDeVideoJuegos2-FINAL/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Use this for initialization
+    public Camera mainCamera;
+    public float speed;
+    private float x;
+    private float y;
+    private float life = 3;
+    private bool moveForward;
+    private bool moveBack;
+    private bool moveRight;
+    private bool moveLeft;
+    private RaycastHit2D hitForward;
+    private RaycastHit2D hitBack;
+    private RaycastHit2D hitRight;
+    private RaycastHit2D hitLeft;
+    private float relativeOrigin = 0.1f;
+    private float subtractH = 0.05f;
+
+    void Start () {
+        moveForward = true;
+        moveBack = true;
+        moveRight = true;
+        moveLeft = true;
+        x = transform.position.x;
+        y = transform.position.y;
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Raycasting();
+	}
+    public void Raycasting()
+    {
+        
+        hitForward = Physics2D.Raycast(new Vector3(transform.position.x,transform.position.y + relativeOrigin,transform.position.z), Vector2.up,speed/2);
+        if (hitForward.collider != null)
+        {
+            if (hitForward.collider.tag == "NO PASABLE")
+            {
+                moveForward = false;
+            }
+        }
+        else if(hitForward.collider == null)
+        {
+            moveForward = true;
+        }
+
+        hitBack = Physics2D.Raycast(new Vector3(transform.position.x, transform.position.y - relativeOrigin, transform.position.z), Vector2.down, speed / 2);
+        if(hitBack.collider != null)
+        {
+            if(hitBack.collider.tag == "NO PASABLE")
+            {
+                moveBack = false;
+            }
+        }
+        else if(hitBack.collider == null)
+        {
+            moveBack = true;
+        }
+
+        hitLeft = Physics2D.Raycast(new Vector3(transform.position.x - relativeOrigin, transform.position.y-subtractH, transform.position.z), Vector2.left, speed / 2);
+        if(hitLeft.collider != null)
+        {
+            if(hitLeft.collider.tag == "NO PASABLE")
+            {
+                moveLeft = false;
+            }
+        }
+        else if(hitLeft.collider == null)
+        {
+            moveLeft = true;
+        }
+
+        hitRight = Physics2D.Raycast(new Vector3(transform.position.x + relativeOrigin, transform.position.y-subtractH, transform.position.z), Vector2.right, speed / 2);
+        if(hitRight.collider != null)
+        {
+            if(hitRight.collider.tag == "NO PASABLE")
+            {
+                moveRight = false;
+            }
+        }
+        else if(hitRight.collider == null)
+        {
+            moveRight = true;
+        }
+        //Debug.DrawLine(transform.position, Vector3.down);
+    }
+    public void Movement()
+    {
+        if (moveForward)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                y = y + speed;
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            }
+        }
+        if (moveBack)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                y = y - speed;
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+
+            }
+        }
+        if (moveLeft)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                x = x - speed;
+                transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            }
+        }
+        if (moveRight)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                x = x + speed;
+                transform.position = new Vector3(x, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
