87dc403e4591c57e809f199489021aaa92c88c6b
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-07 20:19:50 -0300

    Menu de pausa en progreso...

--- Frogger-Clon_Juego/Assets/Scripts/Managers/GameManager.cs
+++ Frogger-Clon_Juego/Assets/Scripts/Managers/GameManager.cs
@@ -7,14 +7,64 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     // Use this for initialization
+    [HideInInspector]
+    public static GameManager InstanceGameManager;
+    [HideInInspector]
+    public bool pause;
+    public GameObject camvasMenuPause;
+    public GameObject camvasPlayer;
     private void Start()
     {
-        
+        InstanceGameManager = this;
+        pause = false;
+        camvasMenuPause.SetActive(false);
+        //CursorOff();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        
-	}
+        CheckCursor();
+        CheckPause();
+    }
+    public void CheckCursor()
+    {
+        if(Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown(KeyCode.C))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    public void CursorOff()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void CheckPause()
+    {
+        if(pause)
+        {
+            Time.timeScale = 0;
+        }
+        if(!pause)
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void SetPause(bool _pause)
+    {
+        pause = _pause;
+        if(pause)
+        {
+            camvasMenuPause.SetActive(true);
+        }
+        else
+        {
+            camvasMenuPause.SetActive(false);
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
