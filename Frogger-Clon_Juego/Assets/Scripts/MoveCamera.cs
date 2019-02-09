b9364fc6b95c5ad16fea72b36146900c6384a325
TheoLeyenda <theoleyenda1997@gmail.com>
2019-02-08 21:56:51 -0300

    Fase 1 del nivel 1 terminada

--- Frogger-Clon_Juego/Assets/Scripts/MoveCamera.cs
+++ Frogger-Clon_Juego/Assets/Scripts/MoveCamera.cs
@@ -42,8 +42,8 @@ public class MoveCamera : MonoBehaviour {
     {
         if(collision.tag == "Player")
         {
-            Debug.Log("ENTRE");
             boxCollider.isTrigger = false;
+            gameObject.tag = "NO PASABLE";
         }
     }
 }
